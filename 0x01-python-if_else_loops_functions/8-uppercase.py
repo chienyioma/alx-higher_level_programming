76c09f87d60dbfd3d3af64b71b4854759669b3aa
chienyioma <chienyioma19@gmail.com>
2022-09-21 14:14:19 -0700

    Python code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-# Check for lowercase using if statement
         num = ord(i)
         if((num >= 97) and (num <= 122)):
-# Convert lowercase to uppercase
             ch = chr(num - 32)
         else:
             ch = chr(num)
