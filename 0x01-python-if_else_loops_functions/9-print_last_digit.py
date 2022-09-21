423c88089e1a51b4793e958be216d3d024cfb8a8
chienyioma <chienyioma19@gmail.com>
2022-09-21 16:30:59 -0700

    Python code

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if(number < 0):
+        last_digit = (((-1 * number) % 10) * -1)
+    elif(number >= 0):
+        last_digit = number % 10
+    print("{}".format(last_digit))
+    return last_digit
