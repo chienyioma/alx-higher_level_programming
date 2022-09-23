9a102d63926cb06289896be0f0c930949a725f97
chienyioma <chienyioma19@gmail.com>
2022-09-23 07:33:18 -0700

    Python code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
-    import sys.argv
-    count = len(argv) - 1
-    if count == 0:
-        print("{} arguments.".format(count))
-    elif count == 1:
-        print("{} argument:".format(count))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(count))
-    if count >= 1:
-        count = 0
-        for arg in argv:
-            if count != 0:
-                print("{}: {}".format(count, arg))
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
             i += 1
