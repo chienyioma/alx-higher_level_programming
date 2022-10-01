f6b90b9080766a1f50476b59abda31716ac0b850
chienyioma <chienyioma19@gmail.com>
2022-09-30 22:32:57 -0700

    Python code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    mul = list((map(lambda t: t[0] * t[1],  my_list)))
-    sumofmul = sum(mul)
-    weight = list(map(lambda t: t[1], my_list))
-    sumofweight = sum(weight)
-    Average = sumofmul / sumofweight
-    return Average
+    if my_list == []:
+        return 0
+    else:
+        mul = list((map(lambda t: t[0] * t[1],  my_list)))
+        sumofmul = sum(mul)
+        weight = list(map(lambda t: t[1], my_list))
+        sumofweight = sum(weight)
+        Average = sumofmul / sumofweight
+        return Average
