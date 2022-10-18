33ffe2685ced890cb47a0f015dfebb4f1aff6480
chienyioma <chienyioma19@gmail.com>
2022-10-18 11:29:07 -0700

    Python code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square class to represent a square"""
+
+
 class Square:
-    """Class Square that defines a square."""
+    """
+    Class Square that defines a square and its basic properties
+    """
     pass
