6a753c7fee5d199f2769db7b7d92d1d90fb18e44
chienyioma <chienyioma19@gmail.com>
2022-10-26 04:06:31 -0700

    Python code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Defines the modeule."""
+""" Defines an inherited class-checking function."""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
     Args:
         obj: object
@@ -12,7 +12,7 @@ def is_kind_of_class(obj, a_class):
         False, otherwise
     """
 
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
