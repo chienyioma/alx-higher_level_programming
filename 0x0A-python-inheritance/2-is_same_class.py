86d8ff292b2261bed5bfc3860ab37aa542d1d97b
chienyioma <chienyioma19@gmail.com>
2022-10-26 03:48:14 -0700

    Python code

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines the module."""
+
+
+def is_same_class(obj, a_class):
+    """ Function that returns True/False if obj is a type of a_class."""
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
